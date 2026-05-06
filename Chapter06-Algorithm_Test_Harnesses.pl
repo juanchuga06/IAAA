5fbe19098d427f60875a574134bddf860d438e3c
Juan Camilo Chuga Rosero <juan.chuga@epn.edu.ec>
2026-05-06 12:10:17 -0500

    IAAA

--- Chapter06-Algorithm_Test_Harnesses.pl
+++ Chapter06-Algorithm_Test_Harnesses.pl
@@ -107,5 +107,3 @@ my ($scores, $train_losses, $test_losses, $actuals, $predictions) = sml->evaluat
                                                                                                                     metric => 'accuracy');
 printf "Scores: %s\n", dump @$scores;
 printf "Mean Accuracy: %0.2f%%", sum(@$scores) / @$scores;
-
-#Hola esto es un comentario
